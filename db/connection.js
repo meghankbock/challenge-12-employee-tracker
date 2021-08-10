81fe8a961f96b5bed6e64ccb8fdb2b335d15b914
Meghan Bock <meghan.bock@gmail.com>
2021-08-09 20:22:57 -0500

    setup dotenv package for storing sql credentials

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql2");
 
+require('dotenv').config();
+
+console.log(process.env.DB_NAME);
+
 // connect to database
 const db = mysql.createConnection(
     {
       host: "localhost",
       user: "root",
-      password: "password",
-      database: "organization",
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
     },
     console.log("Connected to the organization database.")
   );
